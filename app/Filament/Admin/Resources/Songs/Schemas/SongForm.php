647c6e38058eaa06d7de51ed1f7dca5506cb596a
3mm404 <perezsanchezchristianemmanuel@gmail.com>
2026-09-25 11:59:20 -0500

    Change: creado  directorios temporales de PHP para subir archivos MP3 y WAV

--- app/Filament/Admin/Resources/Songs/Schemas/SongForm.php
+++ app/Filament/Admin/Resources/Songs/Schemas/SongForm.php
@@ -18,21 +18,44 @@ class SongForm
 
                 TextInput::make('artist')
                     ->maxLength(255),
+                //aqui podre cambiar de lugar el file upload para que quede en la parte de abajo del formulario
+                //ajustar para la base de datos y el modelo para que acepte el campo file_path como un S3 
+     FileUpload::make('file_path')
+    ->label('Audio file')
 
-                FileUpload::make('file_path')
-                    ->label('Audio file')
-                    ->disk('public')
-                    ->directory('songs')
-                    ->visibility('public')
-                    ->acceptedFileTypes([
-                        'audio/mpeg',
-                        'audio/mp3',
-                        'audio/wav',
-                        'audio/x-wav',
-                        'audio/wave',
-                    ])
-                    ->maxSize(61440)
-                    ->required(),
+    /*
+    |--------------------------------------------------------------------------
+    | Storage
+    |--------------------------------------------------------------------------
+    |
+    | LOCAL / VPS:
+    | ->disk('public')
+    | Los archivos se almacenan en:
+    | storage/app/public/songs
+    |
+    | S3:
+    | ->disk('s3')
+    | Los archivos se almacenan en el bucket configurado en filesystems.php
+    |
+    | Recomendación futura:
+    | ->disk(config('filesystems.audio_disk'))
+    | Así podremos cambiar entre local, VPS y S3 desde .env.
+    |
+    */
+
+    ->disk('public')
+    ->directory('songs')
+    ->visibility('public')
+
+    ->acceptedFileTypes([
+        'audio/mpeg',
+        'audio/mp3',
+        'audio/wav',
+        'audio/x-wav',
+        'audio/wave',
+    ])
+    ->maxSize(61440)
+    ->required(),
             ]);
     }
 }
